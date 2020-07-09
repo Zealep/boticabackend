b8a5126acc7ddb6776bf6e4f6b30434e436b91be
cristhianpelaez13@gmail.com <noseassapo1478963>
2020-07-09 10:22:30 -0500

    upload project

---
+++ src/main/java/com/zealep/api/salesbackend/SalesbackendApplication.java
@@ -0,0 +1,13 @@
+package com.zealep.api.salesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zealep/api/salesbackend/SalesbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zealep.api.salesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
